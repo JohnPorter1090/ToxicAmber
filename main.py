fddca1d30e52a12de515a71927ba65cf27efb3d9
JohnPorter1090 <101543357+JohnPorter1090@users.noreply.github.com>
2022-05-01 22:36:06 -0600

    New Feature "Title Screen!"

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ BACKGROUND = pygame.Rect(0, 0, WIDTH, HEIGHT)
 
 TOXICAMBER_IMAGE = pygame.image.load(os.path.join('images', 'ToxicAmberLogo.png'))
 
+TITLESCREEN = pygame.transform.scale(pygame.image.load(os.path.join('images', 'ToxicAmberTitleScreenNoLetter.png')), (WIDTH, HEIGHT))
 
 CHARACTER_HEIGHT, CHARACTER_WIDTH = 10, 10                                               #Changes the Character Size
 CHARACTER_IMAGE = pygame.image.load(os.path.join('images', 'Character.png'))
@@ -48,11 +49,11 @@ pygame.display.set_icon(TOXICAMBER_IMAGE)
 X_CENTER, Y_CENTER = WIDTH/2, HEIGHT/2 
 
 
-CHARACTER_VEL = 10 #Changes the Characters Speed
+CHARACTER_VEL = 5 #Changes the Characters Speed
 FPS = 60
 
 def form_window(character):
-    pygame.draw.rect(WIN, BLACK, BACKGROUND)
+    WIN.blit(TITLESCREEN, (0, 0))
     WIN.blit(CHARACTER, (character.x, character.y))
     
 
@@ -61,20 +62,28 @@ def form_window(character):
 def handle_character_movement(keys_pressed, character):
     if keys_pressed[pygame.K_a] and character.x - CHARACTER_VEL > 0:
         character.x -= CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
     if keys_pressed[pygame.K_d] and character.x + CHARACTER_VEL + character.width < WIDTH:
         character.x += CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
     if keys_pressed[pygame.K_w] and character.y - CHARACTER_VEL > 0:
         character.y -= CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
     if keys_pressed[pygame.K_s] and character.y + CHARACTER_VEL + character.height < HEIGHT:
         character.y += CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
     if keys_pressed[pygame.K_LEFT] and character.x - CHARACTER_VEL > 0:
         character.x -= CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
     if keys_pressed[pygame.K_RIGHT] and character.x + CHARACTER_VEL + character.width < WIDTH:
         character.x += CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
     if keys_pressed[pygame.K_UP] and character.y - CHARACTER_VEL > 0:
         character.y -= CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
     if keys_pressed[pygame.K_DOWN] and character.y + CHARACTER_VEL + character.height < HEIGHT:
         character.y += CHARACTER_VEL
+        print("Character X: " + str(character.x) + " Character Y: " +str(character.y))
 
 
 def main():
@@ -92,10 +101,13 @@ def main():
                     run = False
                 if event.key == pygame.K_F8:
                     WIN = pygame.display.set_mode((WIDTH, HEIGHT), fullscreen_display)
+                    TOXICAMBER_IMAGE = pygame.image.load(os.path.join('images', 'ToxicAmberLogo.png'))
                 if event.key == pygame.K_F7:
                     WIN = pygame.display.set_mode((WIDTH, HEIGHT), default_display)
+                    TOXICAMBER_IMAGE = pygame.image.load(os.path.join('images', 'ToxicAmberLogo.png'))
                 if event.key == pygame.K_F6:
                     WIN = pygame.display.set_mode((WIDTH, HEIGHT), windowless_display)
+                    TOXICAMBER_IMAGE = pygame.image.load(os.path.join('images', 'ToxicAmberLogo.png'))
         keys_pressed = pygame.key.get_pressed() #Checks for keys that are currently being held down
 
         
